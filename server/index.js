eb589fb081f8ac3e115b432b522b17d4ac653870
Blessly Pera <pera.blessly@yahoo.com>
2022-01-23 05:53:13 +0800

    port fix

--- server/index.js
+++ server/index.js
@@ -93,7 +93,7 @@ app.get('/i/*', (request, reply) => {
 
 process.nextTick(async () => {
   try {
-    await app.listen(3000);
+    await app.listen(3000, '0.0.0.0');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
